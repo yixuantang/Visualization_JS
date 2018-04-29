c390764f13ebd9e18c5f1dfed0a83815d8ccacc5
YixuanTang <343552644@qq.com>
2018-04-28 20:19:07 -0400

    upload data

---
+++ Homework3/analysis.py
@@ -0,0 +1,46 @@
+import altair as alt
+import pandas as pd
+#import json
+
+def createChart(data, zipcode):
+    color_expression    = "highlight._vgsid_==datum._vgsid_"
+    color_condition     = alt.ConditionalPredicateValueDef(color_expression, "SteelBlue")
+    highlight_selection = alt.selection_single(name="highlight", empty="all", on="mouseover")
+    #rating_selection    = alt.selection_single(name="rating", empty="all", encodings=['y'])
+    data = data[data['zipcode']==zipcode].nlargest(20, 'num')
+    try:
+        maxCount            = int(data['num'].max())
+    except ValueError:
+        maxCount = 10
+        data = pd.DataFrame([{"name":"undefine", "num":0}])
+
+    return alt.Chart(data)\
+              .mark_bar(stroke="Black")\
+              .encode(
+                  alt.X("num:Q", axis=alt.Axis(title="Restaurants"),
+                    scale=alt.Scale(domain=(0,maxCount))),
+                  alt.Y('name:O', axis=alt.Axis(title="cuisine"),
+                    sort=alt.SortField(field="num", op="argmax")),
+                  alt.ColorValue("LightGrey", condition=color_condition),
+              ).properties(
+                selection = highlight_selection,
+              )
+
+
+def loadData():
+    import urllib.request, json
+    with urllib.request.urlopen("https://raw.githubusercontent.com/lingyielia/D3-visual/master/data/nyc_restaurants_by_cuisine.json") as url:
+        cuisines = json.loads(url.read().decode())
+
+    tmp_list = []
+    tmp_dict = {}
+    for one in cuisines:
+        tmp_dict['name'] = one['cuisine']
+        for zipcode,num in one['perZip'].items():
+            tmp_dict['zipcode'] = zipcode
+            tmp_dict['num'] = num
+            tmp_list.append(tmp_dict.copy())
+
+    df = pd.DataFrame(tmp_list)
+    return df
+
